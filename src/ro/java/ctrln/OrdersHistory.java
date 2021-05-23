640fe43a9b7bea45a8275ef8de20c8523e8c7f20
cumpa_000 <cumpanasoiu.ui>
2021-05-23 17:17:03 +0300

    Homework abstractizare

--- src/ro/java/ctrln/OrdersHistory.java
+++ src/ro/java/ctrln/OrdersHistory.java
@@ -11,45 +11,6 @@ public class OrdersHistory extends UserAccount {
     OrdersHistory ordersHistory = new OrdersHistory(firstOrder, secondOrder, thierdOrder);
 
     public OrdersHistory(String firstOrder, String secondOrder, String thierdOrder) {
-        this.firstOrder = firstOrder;
-        this.secondOrder = secondOrder;
-        this.thierdOrder = thierdOrder;
-    }
 
-    public String getFirstOrder() {
-        return firstOrder;
-    }
-
-    public void setFirstOrder(String firstOrder) {
-        this.firstOrder = firstOrder;
-    }
-
-    public OrdersHistory getOrdersHistory() {
-        return ordersHistory;
-    }
-
-    public void setOrdersHistory(OrdersHistory ordersHistory) {
-        this.ordersHistory = ordersHistory;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        OrdersHistory that = (OrdersHistory) o;
-        return ordersHistory.equals(that.ordersHistory);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), ordersHistory);
-    }
-
-    @Override
-    public String toString() {
-        return "OrdersHistory{" +
-                "ordersHistory=" + ordersHistory +
-                '}';
     }
 }
